9b414c61855bb6906b30352744a1e4925fe3aa13
marstonad20 <marstonad20@gcc.edu>
2023-03-31 17:12:53 -0400

    Will not compile

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -11,7 +11,7 @@ public class CaesarCipher extends Cipher {
 	}
 	
 	public CaesarCipher(CaesarCipher other){
-		// TODO: complete this copy constructor
+		shiftAmount = other.shiftAmount;
 	}
 	
 	@Override
--- src/Cipher.java
+++ src/Cipher.java
@@ -1,13 +1,16 @@
 public abstract class Cipher {
     public Cipher() {
     }
-    public abstract Cipher newCopy(Cipher cipher);
+    public abstract Cipher newCopy();
     public abstract char encrypt(char c);
     public abstract char decrypt(char c);
+
     public String encrypt(String s) {
+        //TODO: implement
         return "";
     }
     public String decrypt(String s) {
+        //TODO: implement
         return "";
     }
 }
