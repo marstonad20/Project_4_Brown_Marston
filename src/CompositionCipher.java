0ee75fb6e54af5b9130a7a3b635ce781869c9b08
marstonad20 <marstonad20@gcc.edu>
2023-04-15 10:23:55 -0400

    Fleshed out CompositionCipher

--- src/CompositionCipher.java
+++ src/CompositionCipher.java
@@ -1,3 +1,40 @@
-public class CompositionCipher extends Cipher {
+import java.util.ArrayList;
 
+public class CompositionCipher extends Cipher {
+    ArrayList<Cipher> ciphers;
+
+    public CompositionCipher() {
+        ciphers = new ArrayList<Cipher>();
+    }
+
+    public CompositionCipher(CompositionCipher other) {
+        this.ciphers = new ArrayList<Cipher>();
+        for (Cipher otherCipher : other.ciphers) {
+            this.ciphers.add(otherCipher.newCopy());
+        }
+    }
+
+    @Override
+    public char encrypt(char c) {
+        for (int i = 0; i < ciphers.size(); i++) {
+            c = ciphers.get(i).encrypt(c);
+        }
+        return c;
+    }
+    @Override
+    public char decrypt(char c) {
+        for (int i = ciphers.size() - 1; i >= 0; i--) {
+            c = ciphers.get(i).decrypt(c);
+        }
+        return c;
+    }
+
+    @Override
+    public Cipher newCopy() {
+        return new CompositionCipher(this);
+    }
+
+    public void add(Cipher theCipher) {
+        ciphers.add(theCipher);
+    }
 }
